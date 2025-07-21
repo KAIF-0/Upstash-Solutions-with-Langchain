96e43c2656ae05dff6885ae9580d9a6f33e6571c
kaif-0 <kaif8700979251@gmail.com>
2025-07-22 00:47:23 +0530

    added: multi agents chaining

--- main.ts
+++ main.ts
@@ -1,19 +1,54 @@
+import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableLambda, RunnableSequence } from "@langchain/core/runnables";
 import { serve } from "bun";
+import { model } from "./config/gemini";
 
+//multi agents chaining
 const server = serve({
   port: 8000,
+  idleTimeout: 100,
   async fetch(request) {
     const { pathname } = new URL(request.url);
 
-    let response = await request.body;
+    let response = null;
 
-    if (pathname == "/api") {
-      console.log(response);
+    if (pathname == "/blog") {
+      const outlinePrompt = PromptTemplate.fromTemplate(
+        `Create a blog outline on the topic: {topic}`
+      );
+      const outlineChain = RunnableSequence.from([
+        outlinePrompt,
+        model,
+        RunnableLambda.from((output) => output.content),
+      ]);
+
+      const blogPrompt = PromptTemplate.fromTemplate(
+        `Write a full blog post based on this outline: {outline}`
+      );
+      const blogChain = RunnableSequence.from([
+        blogPrompt,
+        model,
+        RunnableLambda.from((output) => output.content),
+      ]);
+
+      //final Chain
+      const agentChain = RunnableSequence.from([
+        RunnableLambda.from((input) => ({ topic: input })),
+        outlineChain,
+        RunnableLambda.from((outline) => ({ outline })),
+        blogChain,
+      ]);
+
+      const topic = "Javascript vs Typescript";
+      response = await agentChain.invoke({ topic });
     }
 
-    return new Response("Hello", {
-      status: 200,
-    });
+    return new Response(
+      response ? response : "Hello from Multi Agents Chaining Service!",
+      {
+        status: 200,
+      }
+    );
   },
   error(error: Error) {
     return new Response("Something Went Wrong: " + error.message, {
